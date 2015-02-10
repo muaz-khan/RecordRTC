59ca77f7d0c8163533afac621c0c1fdf653187a1
Jim Lowrey <jimlowrey@gmail.com>
2015-02-10 13:09:14 -0800

    changed mergeAudioBuffers to a function declaration to preserve the name during minification.

--- dev/StereoAudioRecorder.js
+++ dev/StereoAudioRecorder.js
@@ -52,7 +52,7 @@ function StereoAudioRecorder(mediaStream, config) {
     };
 
     function mergeLeftRightBuffers(config, callback) {
-        var webWorker = processInWebWorker(function mergeAudioBuffers(config) {
+        function mergeAudioBuffers(config) {
             var leftBuffers = config.leftBuffers;
             var rightBuffers = config.rightBuffers;
             var sampleRate = config.sampleRate;
@@ -171,7 +171,8 @@ function StereoAudioRecorder(mediaStream, config) {
                 buffer: buffer,
                 view: view
             });
-        });
+        }
+        var webWorker = processInWebWorker(mergeAudioBuffers);
 
         webWorker.onmessage = function(event) {
             callback(event.data.buffer, event.data.view);
@@ -182,7 +183,7 @@ function StereoAudioRecorder(mediaStream, config) {
 
     function processInWebWorker(_function) {
         var blob = URL.createObjectURL(new Blob([_function.toString(),
-            'this.onmessage =  function (e) {mergeAudioBuffers(e.data);}'
+            'this.onmessage =  function (e) {' + _function.name + '(e.data);}'
         ], {
             type: 'application/javascript'
         }));

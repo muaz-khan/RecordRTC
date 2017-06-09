52166126f161638558d47a6f7ec50d798eb335c9
Rain Shen <slk49@live.cn>
2017-06-09 01:13:16 -0400

    [StereoAudioRecorder]fix stop callback
    As per the previous comment

--- dev/StereoAudioRecorder.js
+++ dev/StereoAudioRecorder.js
@@ -394,7 +394,7 @@ function StereoAudioRecorder(mediaStream, config) {
             self.length = recordingLength;
 
             if (callback) {
-                callback();
+                callback(self.blob);
             }
 
             isAudioProcessStarted = false;
